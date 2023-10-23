ef229d7e37a48ae03f271bb1f594b2dbdd630153
Бакытбек <bakytbek@MacBook-Pro-Bakytbek.local>
2023-10-23 19:02:42 +0600

    f

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: ['content-type, Authorization'],
-    origin: 'http://localhost:5173',
+    origin: 'http://194.58.90.70:5173/',
     credentials: true,
   });
   await app.listen(3000);
