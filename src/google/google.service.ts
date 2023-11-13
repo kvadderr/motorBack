94ccdeef3e0ff65e751f2fcb9444ef41b8e01350
Бакытбек <bakytbek@MacBook-Pro-Bakytbek.local>
2023-11-13 12:08:09 +0600

    second fix

--- src/google/google.service.ts
+++ src/google/google.service.ts
@@ -64,7 +64,7 @@ export class GoogleService {
             await drive.permissions.create({
                 fileId: fileId,
                 requestBody: {
-                    role: 'reader',
+                    role: 'writer',
                     type: 'anyone',
                 },
             });
